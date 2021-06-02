e8f473f7ea6389e08464444d64cb10bc1e9c3b0b
Jeff <11721615+jeffellin@users.noreply.github.com>
2021-06-02 01:46:08 -0400

    add a test

--- customer-service/src/test/java/com/example/oauth/controller/CustomerControllerTest.java
+++ customer-service/src/test/java/com/example/oauth/controller/CustomerControllerTest.java
@@ -3,25 +3,18 @@ package com.example.oauth.controller;
 import com.example.oauth.api.Customer;
 import com.example.oauth.api.CustomerRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -31,9 +24,8 @@ import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,10 +53,20 @@ class CustomerControllerTest  {
 
         this.mockMvc.perform(get("/").with(authentication(authenticationToken)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)));
+                .andExpect(jsonPath("@", hasSize(1)));
     }
 
+    @Test
+    public void whoami() throws Exception {
 
+        BearerTokenAuthentication authenticationToken = createToken();
+
+        this.mockMvc.perform(get("/whoami").with(authentication(authenticationToken)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("@.hello").value("world"))
+                .andExpect(jsonPath("@.security.authorities",hasSize(1)));
+
+    }
 
 
 
